357fa74fe21384e689d306b0035ab1a306630ef2
SmgTr <59412217+SmgTR@users.noreply.github.com>
2022-07-30 12:10:37 +0200

    settings

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'jest-expo',
+  setupFiles: ['./node_modules/react-native-gesture-handler/jestSetup.js', './setupTest.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: ['node_modules/(?!react-native|react-navigation)/'],
+  moduleNameMapper: { '\\.svg': '<rootDir>/__mocks__/svgMock.js' }
+};
+
+export default config;
